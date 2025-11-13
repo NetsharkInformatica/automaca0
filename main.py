cdcbea6dbc3881abc0f654c162b0aa382b608075
netsharkinformatica <helio.tome2024@gmail.com>
2025-11-13 15:54:10 -0300

    começando automação com  pyautogui

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+
+pyautogui.press('win')
+pyautogui.PAUSE=1
+pyautogui.write('chrome')
+pyautogui.press('enter')
+pyautogui.write('heliotome.com.br')
+pyautogui.press('enter')
