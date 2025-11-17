c7c62232057396c1892865a7ffdb8cb27c054163
netsharkinformatica <helio.tome2024@gmail.com>
2025-11-17 10:08:33 -0300

    começando aula2

---
+++ aula2.py
@@ -0,0 +1,6 @@
+import rpa as r
+
+#r.setup()
+
+r.init()
+r.url('https://www.heliotome.com.br')
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import pyautogui
+import pyautogui as p
 import time
 
 
-pyautogui.press('win')
-pyautogui.PAUSE=1
-pyautogui.write('chrome')
-pyautogui.press('enter')
-pyautogui.write('heliotome.com.br')
-pyautogui.press('enter')
+# pyautogui.press('win')
+# pyautogui.PAUSE=1
+# pyautogui.write('chrome')
+# pyautogui.press('enter')
+# pyautogui.write('heliotome.com.br')
+# pyautogui.press('enter')
+#print(p.position())
+p.hotkey('win','r')
+p.sleep(1)
+p.typewrite('notepad')
+p.sleep(2)
+p.press('enter')
+p.sleep(2)
+p.typewrite('ola, eu sou um bot')
+p.sleep(3)
+p.hotkey('ctrl','s')
+p.sleep(3)
+p.typewrite('teste.txt')
+p.sleep(2)
+p.press('enter')
+p.sleep(3)
+p.hotkey('alt','F4')
